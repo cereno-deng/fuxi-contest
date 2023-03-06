443d21db87821035b49390cb6bd345fec1a44d01
Cereno Deng <cereno.deng@gmail.com>
2023-03-07 01:09:41 +0800

    add template functionality

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def read_root():
   return {"Fuxi": "Contest"}
 
 
-@app.get("/contests/{contest_id}")
-async def read_contest(contest_id: int):
-  return {"contest_id": contest_id}
+@app.get("/contests/{id}", response_class=HTMLResponse)
+async def read_contest(request: Request, id: str):
+  return templates.TemplateResponse("contest.html", {"request": request, "id": id})
